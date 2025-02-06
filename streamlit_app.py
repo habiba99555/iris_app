e0e8c7644eec18e90787faf57a8ebdac64fecaeb
habiba99555 <bonjour2095@gmail.com>
2025-02-06 22:51:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.write("hello world")
 st.title('Mon titre')
-st.button('VALIDER') st.button('ANNULER')
+st.button('VALIDER') 
+st.button('ANNULER')
 
 st.image('Enfant.jpg')
