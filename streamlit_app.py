0c75ac73a4c312ef61ca014128f11c70cc90345a
habiba99555 <bonjour2095@gmail.com>
2025-02-15 18:55:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,19 @@ st.markdown("<h1 style='text-align: center;'>TYPE DE FLEUR </h1>", unsafe_allow_
 col1, col2 = st.columns(2)
 
 with col1:
-    sepal = st.text_area("Longueur du sépal", 0.0, 10.0, value=0.0, step=0.5)
-    sepal_length = st.slider("Longueur du sépal", 0.0, 10.0, value=0.0, step=0.5)
+    sepal = st.text_area("Longueur du sépal", 0.0, 10.0, value=0.0, step=0.1)
+    sepal_length = st.slider("Longueur du sépal", 0.0, 10.0, value=0.0, step=0.1)
 
 with col2:
-    sepal_width = st.slider("Largeur du sépal", 0.0, 10.0, value=0.0, step=0.5)
+    sepal_width = st.slider("Largeur du sépal", 0.0, 10.0, value=0.0, step=0.1)
 
 col3, col4 = st.columns(2)
 
 with col3:
-    petal_length = st.slider("Longueur du pétale", 0.0, 10.0, value=0.0, step=0.5)
+    petal_length = st.slider("Longueur du pétale", 0.0, 10.0, value=0.0, step=0.1)
 
 with col4:
-    petal_width = st.slider("Largeur du pétale", 0.0, 10.0, value=0.0, step=0.5)
+    petal_width = st.slider("Largeur du pétale", 0.0, 10.0, value=0.0, step=0.1)
 
 # Bouton pour envoyer les données à l'API
 if st.button("VOIR TYPE FLEUR...", help="Cliquez pour envoyer les données", type="primary"):
